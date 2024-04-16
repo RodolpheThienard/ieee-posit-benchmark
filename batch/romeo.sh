20b6fb157c8af382877ffdb4e8e9febbaa39a8d1
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-04-16 12:09:13 +0200

    Full fix cuda_main & source file to load config

---
+++ batch/romeo.sh
@@ -0,0 +1,3 @@
+module load cuda/11.8 cmake/3.27.9_spack2021_gcc-12.2.0-rvyk gcc/11.3.0
+export LIBRARY_PATH="/apps/2021/cuda/11.8/lib64:${LIBRARY_PATH}"
+export LD_LIBRARY_PATH="/apps/2021/cuda/11.8/lib64:${LD_LIBRARY_PATH}"
--- tests/cuda/main.cu
+++ tests/cuda/main.cu
@@ -1,73 +1,13 @@
 extern "C" {
   #include "../../include/utils.h"
-  // #include "../../include/driver.h"
+  #include "../../include/driver.h"
 }
   #include "../../include/cuda.h"
   #include "../../include/kernels.h"
 
-#define DRIVER_BODY_BENCHMARK_CUDA(kernel, ...)                                         \
-  dim3 threadsPerBlock (32, 32);                                              \
-  dim3 numBlocks (matrix_size / threadsPerBlock.x,                            \
-                  matrix_size / threadsPerBlock.y);                           \
-  cudaEvent_t start_event, end_event;                                         \
-  float elapsed;                                                              \
-  for (uint32_t stability = 0; stability < 33; stability++)                   \
-    {                                                                         \
-      cudaEventCreate (&start_event);                                         \
-      cudaEventCreate (&end_event);                                           \
-      cudaEventRecord (start_event, 0);                                       \
-      for (uint32_t rep = 0; rep < data->repetition; rep++)                   \
-        {                                                                     \
-          kernel<<<numBlocks, threadsPerBlock>>> (__VA_ARGS__);               \
-        }                                                                     \
-      cudaEventRecord (end_event, 0);                                         \
-      cudaEventSynchronize (end_event);                                       \
-      cudaEventElapsedTime (&elapsed, start_event, end_event);                \
-      elapsed *= 1e-3;                                                        \
-      data->samples[stability] = (double)elapsed / data->repetition;          \
-    }                                                                         
-
-void
-driver_cuda_fp64_benchmark (char *title, char *buffer,
-                            void (*kernel) (double *, double *, double *, int),
-                            struct data *data, int matrix_size)
-{
-  double *A, *B, *C, *d_A, *d_B, *d_C;
-  long _matrix_size_2 = matrix_size * matrix_size;
-  A = (double *)malloc (sizeof (double) * _matrix_size_2);
-  B = (double *)malloc (sizeof (double) * _matrix_size_2);
-  C = (double *)malloc (sizeof (double) * _matrix_size_2);
-
-  INIT (A, _matrix_size_2);
-  INIT (B, _matrix_size_2);
-  INIT (C, _matrix_size_2);
-
-  cublasHandle_t handle;
-  cublasCreate (&handle);
-  cudaMalloc (&d_A, _matrix_size_2 * sizeof (double));
-  cudaMalloc (&d_B, _matrix_size_2 * sizeof (double));
-  cudaMalloc (&d_C, _matrix_size_2 * sizeof (double));
-
-  cudaMemcpy (d_A, A, _matrix_size_2 * sizeof (double),
-              cudaMemcpyHostToDevice);
-  cudaMemcpy (d_B, B, _matrix_size_2 * sizeof (double),
-              cudaMemcpyHostToDevice);
-
-  DRIVER_BODY_BENCHMARK_CUDA (kernel, d_A, d_B, d_C, matrix_size);
-  formatting_data (data);
-  print_data_benchmark (title, data, buffer);
-
-  cudaFree (d_A);
-  cudaFree (d_B);
-  cudaFree (d_C);
-  free (A);
-  free (B);
-  free (C);
-}
 int
 main (int argc, char *argv[])
 {
-
   struct data *data;
   data = (struct data*)malloc(sizeof(struct data));
   
@@ -83,14 +23,13 @@ main (int argc, char *argv[])
   output_buffer = (char *)malloc (sizeof (char) * 200 * 11);
   print_header_benchmark (output_buffer, _matrix_size_2 * 2);
   data->type = sizeof (double);
-  char test[] = "cuda gemm ijk";
 
-  driver_cuda_fp64_benchmark (test, output_buffer, cuda_64bits_gemm_ijk, data,
+  driver_cuda_fp64_benchmark ("cuda gemm ijk", output_buffer, cuda_64bits_gemm_ijk, data,
                               _matrix_size);
-  // driver_cuda_fp64_benchmark ("cuda gemm jik", output_buffer, cuda_64bits_gemm_jik, data, 
-  //                // N);
-  // driver_cuda_fp64_benchmark ("cuda gemm bloc", output_buffer, cuda_64bits_gemm_bloc, data, 
-  //                // N);
+  driver_cuda_fp64_benchmark ("cuda gemm jik", output_buffer, cuda_64bits_gemm_jik, data, 
+                 _matrix_size);
+  driver_cuda_fp64_benchmark ("cuda gemm bloc", output_buffer, cuda_64bits_gemm_bloc, data, 
+                 _matrix_size);
 
   save_data (NULL, output_buffer);
 
