4938ebd8054664fdbaa09b03b00406d547d32b76
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-04-17 13:02:33 +0200

    Conversion calculation for OMP

---
+++ kernels/omp/conversion.c
@@ -0,0 +1,18 @@
+#include "../../include/utils.h"
+
+void
+conversion_double_float (double *a, int n)
+{
+  float tmp[n];
+#pragma omp for
+  for (int i = 0; i < n; i++)
+    {
+      tmp[i] = (float)a[i];
+      a[i] = (double)tmp[i];
+    }
+}
+
+void
+useless_function (double *a, int i)
+{
+}
