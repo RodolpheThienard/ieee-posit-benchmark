2fd316e17173ff9b66ff621210003de3444c65db
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-04-22 15:49:50 +0200

    Several test for the API
    SQRT approx, Sinus approx, inverse matrix & BLAS SQRT

--- include/kernels.h
+++ include/kernels.h
@@ -21,8 +21,7 @@ void ieee_64bits_mul (double *matrix1, double *matrix2, double *matrix3,
                       int size);
 void ieee_64bits_div (double *matrix1, double *matrix2, double *matrix3,
                       int size);
-void ieee_64bits_sqrt (double *matrix1, double *matrix2, double *matrix3,
-                       int size);
+void ieee_64bits_sqrt (double *matrix1, double *matrix2, int size);
 void ieee_64bits_dp (double *matrix1, double *matrix2, double *matrix3,
                      int size);
 void ieee_64bits_gemm_ijk (double *matrix1, double *matrix2, double *matrix3,
@@ -38,10 +37,10 @@ void logarithm_taylor (double *vector, int size);
 void log_libmath (double *vector, int size);
 void conversion_double_float (double *vector, int size);
 void useless_function (double *vector, int size);
-void square_root_newton_raphson (double *vector, int size);
-void sqrt_libmath (double *vector, int size);
-void sinus_maclaurin (double *vector, int size);
-void sinus_libmath (double *vector, int size);
+void square_root_newton_raphson (double *input, double *output, int size);
+void sqrt_libmath (double *input, double *output, int size);
+void sinus_maclaurin (double *input, double *output, int size);
+void sinus_libmath (double *input, double *output, int size);
 void monte_carlo_option_pricing_fp64 (double *vector, int n);
 void monte_carlo_option_pricing_fp32 (float *vector, int n);
 void pi_approximation_fp64 (double *vector, int n);
