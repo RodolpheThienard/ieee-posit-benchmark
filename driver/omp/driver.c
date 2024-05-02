c72c1e7fc6f6cc1f9b269e67ac2f8db174dc1160
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-02 17:23:24 +0200

    Update Driver to Bench & accuracy separate the both to be able to compute each correctly

--- driver/omp/driver.c
+++ driver/omp/driver.c
@@ -1,4 +1,5 @@
 #include "../../include/utils.h"
+#include <string.h>
 /* TODO Driver macro bandwidth
    macro permettant de faire la mesure du temps / débit
    paramettre variable
@@ -16,6 +17,7 @@
         for (int rep = 0; rep < bench->data->repetition; rep++)               \
           {                                                                   \
             _Pragma ("omp parallel") { fn (__VA_ARGS__); }                    \
+            _Pragma ("omp barrier")                                           \
           }                                                                   \
         end_register = rdtsc ();                                              \
         end_time = get_elapsedtime ();                                        \
@@ -25,12 +27,17 @@
             = (end_time - init_time) / bench->data->repetition;               \
       }                                                                       \
   }
+#define DRIVER_ACCURACY(fn, ...)                                              \
+  {                                                                           \
+    _Pragma ("omp parallel") { fn (__VA_ARGS__); }                            \
+    _Pragma ("omp barrier")                                                   \
+  }
 
 /* TODO driver macro accuracy
    macro permettant de faire la mesure de précision
    verification de la sorti du calcul de Bandwidth avec la fonction CPU
    Checker si OMP et x86 sont équivalent */
-#define DRIVER_ACCURACY(host, device, size)                                   \
+#define DRIVER_ACCURACY_COMPARE(host, device, size)                           \
   bench->accuracy->accuracy = compute_err_accuracy (host, device, size);      \
   bench->accuracy->RMS = RMS (host, device, size);                            \
   bench->accuracy->forward_error = forward_error (host, device, size);
@@ -42,6 +49,8 @@ driver_sgemm (void (*function) (float *, float *, float *, int), int size,
               float *a, float *b, float *c, struct bench_s bench[static 1])
 {
   DRIVER_BANDWIDTH (function, a, b, c, size);
+  memset (c, 0, size * size * sizeof (float));
+  DRIVER_ACCURACY (function, a, b, c, size);
   formatting_data (bench->data);
 }
 
@@ -49,5 +58,5 @@ void
 driver_accuracy (int size, double *c_host, double *c_device,
                  struct bench_s bench[static 1])
 {
-  DRIVER_ACCURACY (c_host, c_device, size);
+  DRIVER_ACCURACY_COMPARE (c_host, c_device, size);
 }
