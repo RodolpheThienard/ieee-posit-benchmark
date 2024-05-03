f7093fec8e03a73cd1c1e058dfd6669a7ae1ac07
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-03 11:20:41 +0200

    Cmake update & Include
    Adding host kernels in omp compilation
    update kernel host & device api

--- include/kernels.h
+++ include/kernels.h
@@ -16,8 +16,6 @@ void host_inve_matrix_gauss_jordan (double *matrix1, double *matrix2,
                                     int size);
 void host_logarithm_taylor (double *input, double *output, int size);
 void host_log_libmath (double *input, double *output, int size);
-void host_conversion_double_float (double *input, double *output, int size);
-void host_useless_function (double *input, double *output, int size);
 void host_square_root_newton_raphson (double *input, double *output, int size);
 void host_sqrt_libmath (double *input, double *output, int size);
 void host_sinus_maclaurin (double *input, double *output, int size);
@@ -27,8 +25,32 @@ void host_pi_approximation (double *input, double *output, int size);
 void host_real_pi (double *input, double *output, int size);
 
 /// Kernels API
+void vector_add (float *matrix1, float *matrix2, float *matrix3, int size);
+void vector_mul (float *matrix1, float *matrix2, float *matrix3, int size);
+void vector_div (float *matrix1, float *matrix2, float *matrix3, int size);
+void vector_sqrt (float *matrix1, float *matrix2, int size);
+void vector_reduction (float *matrix1, float *matrix2, float *matrix3,
+                       int size);
 void sgemm (float *matrix1, float *matrix2, float *matrix3, int size);
 
+// Matrix inversion
 void inve_matrix_gauss_jordan (float *matrix1, float *matrix2, int size);
-void inve_matrix_gauss_jordan_2 (float *matrix1, float *matrix2, int size,
-                                 int i);
+
+// Logaritm calculation
+void logarithm_taylor (float *input, float *output, int size);
+void log_libmath (float *input, float *output, int size);
+
+// Square root calculation
+void square_root_newton_raphson (float *input, float *output, int size);
+void sqrt_libmath (float *input, float *output, int size);
+
+// Sinus calculation
+void sinus_maclaurin (float *input, float *output, int size);
+void sinus_libmath (float *input, float *output, int size);
+
+// Pi calculation
+void pi_approximation (float *input, float *output, int size);
+void real_pi (float *input, float *output, int size);
+
+// App pricing determination
+void monte_carlo_option_pricing (float *input, float *output, int size);
