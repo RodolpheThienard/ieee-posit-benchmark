c7faae128fb9c78477121a88cca6b5d419f8d10c
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-06-24 10:15:00 +0200

    Rename posits emulation files & Tensor Float 32 emulation

--- implementations/emulations/tests/fastsigmoid/inf.cpp
+++ implementations/emulations/tests/fastsigmoid/inf.cpp
@@ -7,64 +7,31 @@
 #include "../../fastsigmoids/c-src/include/posit.h"
 #include <iostream>
 #include <type_traits>
-using namespace std;
-struct tf32
-{
-  unsigned int data;
-
-public:
-  tf32 () { data = 0; }
-
-  tf32 (float t_floatVal)
-  {
-    data = (*reinterpret_cast<unsigned int *> (&t_floatVal)) >> 13;
-  }
-  tf32 (double t_doubleVal)
-  {
-    float floatVal = (float)t_doubleVal;
-    data = (*reinterpret_cast<unsigned int *> (&floatVal)) >> 13;
-  }
-  // cast to float
-  operator float ()
-  {
-    unsigned int extendedData = data << 13;
-    return *reinterpret_cast<float *> (&extendedData);
-  }
-  // cast to tf32
-  tf32 &
-  operator= (float t_floatVal)
-  {
-    data = (*reinterpret_cast<unsigned int *> (&t_floatVal)) >> 13;
-    return *this;
-  }
-};
 
 int
 main (int argc, char *argv[])
 {
   FILE *file;
-  file = fopen ("tf32_vs_p16_3.dat", "w");
+  file = fopen ("p16_around_0.dat", "w");
 
   double f;
   for (f = 1; f < 1e3; f *= 1.001)
     {
 
-      fprintf (file, "%f; %f; %f; %f; %f; %lf\n", (float)f,
-               (((double)((P16e0)f)) - f) / f, (((double)((tf32)f)) - f) / f,
-               (((double)((P16e1)f)) - f) / f, (((double)((P16e2)f)) - f) / f,
-               f);
+      fprintf (file, "%f; %f; %f; %f; %lf\n", (float)f,
+               (((double)((P16e0)f)) - f) / f, (((double)((P16e1)f)) - f) / f,
+               (((double)((P16e2)f)) - f) / f, f);
     }
   fclose (file);
 
-  file = fopen ("tf32_vs_p16.dat", "w");
+  file = fopen ("p16_range.dat", "w");
 
   for (f = 1; f < 1e40; f *= 2)
     {
 
-      fprintf (file, "%f; %f; %f; %f; %f; %lf\n", (float)f,
-               (((double)((P16e0)f)) - f) / f, (((double)((tf32)f)) - f) / f,
-               (((double)((P16e1)f)) - f) / f, (((double)((P16e2)f)) - f) / f,
-               f);
+      fprintf (file, "%f; %f; %f; %f; %lf\n", (float)f,
+               (((double)((P16e0)f)) - f) / f, (((double)((P16e1)f)) - f) / f,
+               (((double)((P16e2)f)) - f) / f, f);
     }
   fclose (file);
   return 0;
---
+++ implementations/emulations/tests/tensorfloat/inf.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <type_traits>
+using namespace std;
+struct tf32
+{
+  unsigned int data;
+
+public:
+  tf32 () { data = 0; }
+
+  tf32 (float t_floatVal)
+  {
+    data = (*reinterpret_cast<unsigned int *> (&t_floatVal)) >> 13;
+  }
+  tf32 (double t_doubleVal)
+  {
+    float floatVal = (float)t_doubleVal;
+    data = (*reinterpret_cast<unsigned int *> (&floatVal)) >> 13;
+  }
+  // cast to float
+  operator float ()
+  {
+    unsigned int extendedData = data << 13;
+    return *reinterpret_cast<float *> (&extendedData);
+  }
+  // cast to tf32
+  tf32 &
+  operator= (float t_floatVal)
+  {
+    data = (*reinterpret_cast<unsigned int *> (&t_floatVal)) >> 13;
+    return *this;
+  }
+};
+
+int
+main (int argc, char *argv[])
+{
+  FILE *file;
+  file = fopen ("tf32_range.dat", "w");
+
+  double f;
+  for (f = 1; f < 1e3; f *= 1.001)
+    {
+
+      fprintf (file, "%f, %f; %lf\n", (float)f, (((double)((tf32)f)) - f) / f,
+               f);
+    }
+  fclose (file);
+
+  file = fopen ("tf32_around_0.dat", "w");
+
+  for (f = 1; f < 1e40; f *= 2)
+    {
+
+      fprintf (file, "%f; %f; %lf\n", (float)f, (((double)((tf32)f)) - f) / f,
+               f);
+    }
+  fclose (file);
+  return 0;
+}
